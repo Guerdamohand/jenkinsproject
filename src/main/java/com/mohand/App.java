bc9fc137ea5686b81f849063182d362f85626305
Guerda <guerda.mohand93@gmail.com>
2024-09-12 14:45:30 +0200

    first commit

---
+++ src/main/java/com/mohand/App.java
@@ -0,0 +1,13 @@
+package com.mohand;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
